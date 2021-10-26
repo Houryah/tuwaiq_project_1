7a1a97f23d1249ebb883732fd40cb5258a24dbcb
houryah <houryah.thabet.eam.js@tuwaiq.edu.sa>
2021-10-26 17:03:56 +0300

    project1

---
+++ js/contact.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    $('#neom').click(function () {
+
+            sessionStorage.setItem('name', 'neom');
+        })
+        
+$('#redsea').click(function () {
+
+    sessionStorage.setItem('name', 'redsea');
+
+})
+
+$('#Qiddiya').click(function () {
+
+    sessionStorage.setItem('name', 'Qiddiya');
+
+}) 
+
+$('#roshn').click(function () {
+
+    sessionStorage.setItem('name', 'roshn');
+
+})
+
+$('#Diriyah').click(function () {
+
+    sessionStorage.setItem('name', 'Diriyah');
+
+})
+
+$('#amaala').click(function () {
+
+    sessionStorage.setItem('name', 'amaala');
+
+})
+
+
+$('#alulla').click(function () {
+
+    sessionStorage.setItem('name', 'alulla');
+
+
+})
+
+
+
+$('#riyadhart').click(function () {
+
+    sessionStorage.setItem('name', 'riyadhart');
+
+
+})
+
+
+$('#genome').click(function () {
+
+    sessionStorage.setItem('name', 'genome');
+
+
+
+})
+
+
+
+});
+
+
---
+++ js/details.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    let saudi = sessionStorage.getItem('name');
+    $('#' + saudi).removeClass('hidden');
+    // $("#neom")
+})
+
+
+
+$("<svg></svg>").click(function(){
+
+    $("<svg></svg>").css('color','red') 
+})
